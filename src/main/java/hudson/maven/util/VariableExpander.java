f6112cb4e7fece3c5dd48c93a68f61649bad0b7b
André Tadeu de Carvalho <andre.tadeu.de.carvalho@gmail.com>
2017-08-15 23:17:10 -0300

    [JENKINS-13277] Global Environment Variables in Email notification. (#101)
    * [JENKINS-13277] Fix 'Global environment variables are not being resolved in Email Notification Recipients list for maven 2/3 projects'
    
    * Add integration tests.

---
+++ src/main/java/hudson/maven/util/VariableExpander.java
@@ -0,0 +1,37 @@
+package hudson.maven.util;
+
+import hudson.model.AbstractBuild;
+import hudson.model.TaskListener;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public final class VariableExpander {
+
+    private static final Logger LOGGER = Logger.getLogger(VariableExpander.class.getName());
+
+    private final AbstractBuild<?, ?> build;
+    private final TaskListener listener;
+
+    public VariableExpander(AbstractBuild<?, ?> build, TaskListener listener) {
+        if (build == null) {
+            throw new IllegalArgumentException("'build' cannot be null.");
+        }
+        if (listener == null) {
+            throw new IllegalArgumentException("'listener' cannot be null.");
+        }
+        this.build = build;
+        this.listener = listener;
+    }
+
+    public String expand(final String rawString) {
+        try {
+            return build.getEnvironment(listener).expand(rawString);
+        } catch (IOException e) {
+            LOGGER.fine("Cannot expand the variables in email recipients.");
+        } catch (InterruptedException e) {
+            LOGGER.fine("Cannot expand the variables in email recipients.");
+        }
+        return rawString;
+    }
+}
