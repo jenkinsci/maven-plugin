0850af2d727f58b55a0aafcbd14f1b08d9da1f05
olivier lamy <olamy@apache.org>
2019-04-22 21:14:47 +1000

    upgrade maven 3.5.4 avoid using methods non available in the old maven version so copy methods here
    Signed-off-by: olivier lamy <olamy@apache.org>

--- src/main/java/hudson/maven/util/ExecutionEventLogger.java
+++ src/main/java/hudson/maven/util/ExecutionEventLogger.java
@@ -19,11 +19,23 @@ package hudson.maven.util;
  * under the License.
  */
 
+import org.apache.maven.execution.BuildFailure;
+import org.apache.maven.execution.BuildSuccess;
+import org.apache.maven.execution.BuildSummary;
 import org.apache.maven.execution.ExecutionEvent;
+import org.apache.maven.execution.MavenExecutionResult;
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.utils.logging.MessageBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import static org.apache.maven.shared.utils.logging.MessageUtils.buffer;
 
 // Note: copied from package org.apache.maven.cli with just one minor adaption for Maven3Mojo
 public class ExecutionEventLogger
@@ -31,6 +43,9 @@ public class ExecutionEventLogger
 {
     private final Logger logger;
     private final String mojoNote;
+
+    private static final int MAX_PADDED_BUILD_TIME_DURATION_LENGTH = 9;
+    private static final int MAX_PROJECT_NAME_LENGTH = 52;
 
     public ExecutionEventLogger()
     {
@@ -85,6 +100,212 @@ public class ExecutionEventLogger
     private void append( StringBuilder buffer, MavenProject project )
     {
         buffer.append( " @ " ).append( project.getArtifactId() );
+    }
+
+    @Override
+    public void sessionEnded( ExecutionEvent event )
+    {
+        if ( logger.isInfoEnabled() )
+        {
+            if ( event.getSession().getProjects().size() > 1 )
+            {
+                logReactorSummary( event.getSession() );
+            }
+
+            logResult( event.getSession() );
+
+            logStats( event.getSession() );
+
+            infoLine( '-' );
+        }
+    }
+
+    //----------------------------------------------------------------
+    // This is ugly/hackhish/etc  but the classloader mechanism used
+    // need this as some classes from maven core are loaded by jenkins
+    // master for parsing poms so we need to avoid the new methods
+    // from maven core
+    //----------------------------------------------------------------
+
+    private void logReactorSummary( MavenSession session )
+    {
+        infoLine( '-' );
+
+        infoMain( "Reactor Summary:" );
+
+        logger.info( "" );
+
+        MavenExecutionResult result = session.getResult();
+
+        List<MavenProject> projects = session.getProjects();
+        MavenProject lastProject = projects.get( projects.size() - 1 );
+        MavenProject topProject = session.getTopLevelProject();
+
+        for ( MavenProject project : projects )
+        {
+            StringBuilder buffer = new StringBuilder( 128 );
+
+            buffer.append( project.getName() );
+            buffer.append( ' ' );
+
+            if ( topProject.equals( project ) || lastProject.equals( project )
+                || !topProject.getVersion().equals( project.getVersion() ) )
+            {
+                buffer.append( project.getVersion() );
+                buffer.append( ' ' );
+            }
+
+            if ( buffer.length() <= MAX_PROJECT_NAME_LENGTH )
+            {
+                while ( buffer.length() < MAX_PROJECT_NAME_LENGTH )
+                {
+                    buffer.append( '.' );
+                }
+                buffer.append( ' ' );
+            }
+
+            BuildSummary buildSummary = result.getBuildSummary( project );
+
+            if ( buildSummary == null )
+            {
+                buffer.append( buffer().warning( "SKIPPED" ) );
+            }
+            else if ( buildSummary instanceof BuildSuccess )
+            {
+                buffer.append( buffer().success( "SUCCESS" ) );
+                buffer.append( " [" );
+                String buildTimeDuration = formatDuration( buildSummary.getTime() );
+                int padSize = MAX_PADDED_BUILD_TIME_DURATION_LENGTH - buildTimeDuration.length();
+                if ( padSize > 0 )
+                {
+                    buffer.append( chars( ' ', padSize ) );
+                }
+                buffer.append( buildTimeDuration );
+                buffer.append( ']' );
+            }
+            else if ( buildSummary instanceof BuildFailure )
+            {
+                buffer.append( buffer().failure( "FAILURE" ) );
+                buffer.append( " [" );
+                String buildTimeDuration = formatDuration( buildSummary.getTime() );
+                int padSize = MAX_PADDED_BUILD_TIME_DURATION_LENGTH - buildTimeDuration.length();
+                if ( padSize > 0 )
+                {
+                    buffer.append( chars( ' ', padSize ) );
+                }
+                buffer.append( buildTimeDuration );
+                buffer.append( ']' );
+            }
+
+            logger.info( buffer.toString() );
+        }
+    }
+
+    private static final int LINE_LENGTH = 72;
+
+    private static final long ONE_SECOND = 1000L;
+
+    private static final long ONE_MINUTE = 60 * ONE_SECOND;
+
+    private static final long ONE_HOUR = 60 * ONE_MINUTE;
+
+    private static final long ONE_DAY = 24 * ONE_HOUR;
+
+    private static String formatDuration( long duration )
+    {
+        // CHECKSTYLE_OFF: MagicNumber
+        long ms = duration % 1000;
+        long s = ( duration / ONE_SECOND ) % 60;
+        long m = ( duration / ONE_MINUTE ) % 60;
+        long h = ( duration / ONE_HOUR ) % 24;
+        long d = duration / ONE_DAY;
+        // CHECKSTYLE_ON: MagicNumber
+
+        String format;
+        if ( d > 0 )
+        {
+            // Length 11+ chars
+            format = "%d d %02d:%02d h";
+        }
+        else if ( h > 0 )
+        {
+            // Length 7 chars
+            format = "%2$02d:%3$02d h";
+        }
+        else if ( m > 0 )
+        {
+            // Length 9 chars
+            format = "%3$02d:%4$02d min";
+        }
+        else
+        {
+            // Length 7-8 chars
+            format = "%4$d.%5$03d s";
+        }
+
+        return String.format( format, d, h, m, s, ms );
+    }
+
+    private void logResult( MavenSession session )
+    {
+        infoLine( '-' );
+        MessageBuilder buffer = buffer();
+
+        if ( session.getResult().hasExceptions() )
+        {
+            buffer.failure( "BUILD FAILURE" );
+        }
+        else
+        {
+            buffer.success( "BUILD SUCCESS" );
+        }
+        logger.info( buffer.toString() );
+    }
+
+    private void logStats( MavenSession session )
+    {
+        infoLine( '-' );
+
+        long finish = System.currentTimeMillis();
+
+        long time = finish - session.getRequest().getStartTime().getTime();
+
+        // new methods from maven core so we ignore
+        // String wallClock = session.getRequest().getDegreeOfConcurrency() > 1 ? " (Wall Clock)" : "";
+
+        String wallClock = "";
+
+        logger.info( "Total time: " + formatDuration( time ) + wallClock );
+
+        logger.info( "Finished at: " + formatTimestamp( finish ) );
+    }
+
+    private void infoLine( char c )
+    {
+        infoMain( chars( c, LINE_LENGTH ) );
+    }
+
+    private void infoMain( String msg )
+    {
+        logger.info( buffer().strong( msg ).toString() );
+    }
+
+    private static String chars( char c, int count )
+    {
+        StringBuilder buffer = new StringBuilder( count );
+
+        for ( int i = count; i > 0; i-- )
+        {
+            buffer.append( c );
+        }
+
+        return buffer.toString();
+    }
+
+    public static String formatTimestamp( long timestamp )
+    {
+        SimpleDateFormat sdf = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ssXXX" );
+        return sdf.format( new Date( timestamp ) );
     }
 
 }
