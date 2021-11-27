871e832cd5f0351a000620f51f27576ebce58b0b
Basil Crow <me@basilcrow.com>
2021-11-26 21:21:30 -0800

    Move tests from core

---
+++ src/test/java/hudson/tasks/EnvVarsInConfigTasksTest.java
@@ -0,0 +1,93 @@
+package hudson.tasks;
+
+import hudson.EnvVars;
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.model.JDK;
+import hudson.model.Result;
+import hudson.model.labels.LabelAtom;
+import hudson.slaves.DumbSlave;
+import hudson.tasks.Maven.MavenInstallation;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.jvnet.hudson.test.BuildWatcher;
+import org.jvnet.hudson.test.ExtractResourceSCM;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.ToolInstallations;
+
+public class EnvVarsInConfigTasksTest {
+    public static final String DUMMY_LOCATION_VARNAME = "TOOLS_DUMMY_LOCATION";
+
+    private DumbSlave agentEnv = null;
+    private DumbSlave agentRegular = null;
+
+    @ClassRule public static BuildWatcher buildWatcher = new BuildWatcher();
+
+    @Rule public JenkinsRule j = new JenkinsRule();
+
+    @Rule public TemporaryFolder tmp = new TemporaryFolder();
+
+    @Before
+    public void setUp() throws Exception {
+        JDK defaultJDK = j.jenkins.getJDK(null);
+        JDK varJDK = new JDK("varJDK", withVariable(defaultJDK.getHome()));
+        j.jenkins.getJDKs().add(varJDK);
+
+        // Maven with a variable in its path
+        ToolInstallations.configureDefaultMaven();
+        MavenInstallation defaultMaven =
+                j.jenkins.getDescriptorByType(Maven.DescriptorImpl.class).getInstallations()[0];
+        MavenInstallation varMaven =
+                new MavenInstallation(
+                        "varMaven",
+                        withVariable(defaultMaven.getHome()),
+                        JenkinsRule.NO_PROPERTIES);
+        j.jenkins.getDescriptorByType(Maven.DescriptorImpl.class).setInstallations(varMaven);
+
+        // create agents
+        EnvVars additionalEnv = new EnvVars(DUMMY_LOCATION_VARNAME, "");
+        agentEnv = j.createSlave(new LabelAtom("agentEnv"), additionalEnv);
+        agentRegular = j.createSlave(new LabelAtom("agentRegular"));
+    }
+
+    private String withVariable(String s) {
+        return s + "${" + DUMMY_LOCATION_VARNAME + "}";
+    }
+
+    @Test
+    public void testFreeStyleMavenOnAgent() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        project.setJDK(j.jenkins.getJDK("varJDK"));
+        project.setScm(new ExtractResourceSCM(getClass().getResource("/simple-projects.zip")));
+
+        project.getBuildersList()
+                .add(
+                        new Maven(
+                                "test",
+                                "varMaven",
+                                "pom.xml${" + DUMMY_LOCATION_VARNAME + "}",
+                                "",
+                                "",
+                                false));
+
+        // test the regular agent - variable not expanded
+        project.setAssignedLabel(agentRegular.getSelfLabel());
+        FreeStyleBuild build = project.scheduleBuild2(0).get();
+
+        j.assertBuildStatus(Result.FAILURE, build);
+
+        j.assertLogContains(DUMMY_LOCATION_VARNAME, build);
+
+        // test the agent with prepared environment
+        project.setAssignedLabel(agentEnv.getSelfLabel());
+        build = project.scheduleBuild2(0).get();
+
+        j.assertBuildStatusSuccess(build);
+
+        // Check variable was expanded
+        j.assertLogNotContains(DUMMY_LOCATION_VARNAME, build);
+    }
+}
